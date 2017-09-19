af6487c7cb14c1babde888cdd1975e05001d84ab
arju88nair <arju88nair@gmail.com>
2017-09-19 22:58:55 +0530

    This is a mess.Completed crude adaptation of api calls,No idea how to clean this

--- main.py
+++ main.py
@@ -1,51 +1,78 @@
 #!/usr/bin/env python
 import feedparser # pip install feedparser
-import asyncio
+from pymongo import MongoClient
+import json
+import re
+import datetime
+
+def Type1parser(url,source,category,tag):
+    """
+
+    This class handles all the feed parsing jobs initialted by the main function
+
+    """
 
 
-async def get(url):
-    print("Start: %s" % url)
-    response = await feedparser.parse(url)
-    print("Finish: %s" % url)
-    response.close()
-    return 1
+    feed = feedparser.parse(url)
+    posts = []
+    for item in feed['entries']:
 
 
-async def run_all(url1, url2):
-    r1 = get(url1)
-    r2 = get(url2)
-    results = await asyncio.gather(r1, r2)
+        summarys=""
+        if 'summary' in item:
+            summarys=item.summary
+        publishedTag=""
+        if 'published' in item:
+            publishedTag=item.published
+        if 'media_content' in item:
+             posts.append({
+            'title': item.title,
+            'summary': summarys,
+            'link': item.link,
+            'image':item.media_content[0]['url'],
+            'published':publishedTag,
+            'source':source,
+            'category':category,
+            'type':tag
+        })
 
-    for res in results:
-        print(res)
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run_all("http://rss.cnn.com/rss/edition.rss", "http://rss.cnn.com/rss/edition_world.rss"))
-loop.close()
-
-# from pymongo import MongoClient
-
-# def Type1parser(url,source,category,type):
-#     d = feedparser.parse(url)
-
-#     for item in d['items']:
-#         if  'description' in item:
-#             print(item['description'])
-
-
-#         print("\n")
-
-
-
-
-# def main():
-#     Type1parser("http://rss.cnn.com/rss/edition.rss","CNN","General","Latest")
+        print(item.title)
+    # print(json.dumps(posts))
 
 
 
 
 
 
-# if __name__  ==  '__main__':
-#     main()
+
+def main():
+    """  Calling the main class parser for appropriate  rss feeds """
+
+
+    Type1parser("http://rss.cnn.com/rss/edition.rss","CNN","General","Top")
+    Type1parser("http://rss.cnn.com/rss/edition_world.rss","CNN","World","Top")
+    Type1parser("http://rss.cnn.com/rss/edition_technology.rss","CNN","Technology","Top")
+    Type1parser("http://rss.cnn.com/rss/edition_space.rss","CNN","Science","Top")
+    Type1parser("http://rss.cnn.com/rss/edition_entertainment.rss","CNN","Entertainment","Top")
+    Type1parser("http://rss.cnn.com/rss/cnn_latest.rss","CNN","General","Latest")
+    print("\n")
+
+
+    Type1parser("http://feeds.bbci.co.uk/news/rss.xml","BBC","General","Top")
+    Type1parser("http://feeds.bbci.co.uk/news/world/rss.xml","BBC","World","Top")
+    Type1parser("http://feeds.bbci.co.uk/news/politics/rss.xml","BBC","Politics","Top")
+    Type1parser("http://feeds.bbci.co.uk/news/business/rss.xml","BBC","Business","Top")
+    Type1parser("http://feeds.bbci.co.uk/news/health/rss.xml","BBC","Health","Top")
+    Type1parser("http://feeds.bbci.co.uk/news/education/rss.xml","BBC","Education","Top")
+    Type1parser("http://feeds.bbci.co.uk/news/science_and_environment/rss.xml","BBC","Science","Top")
+    Type1parser("http://feeds.bbci.co.uk/news/technology/rss.xml","BBC","Technology","Top")
+    Type1parser("http://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml","BBC","Entertainment","Top")
+
+
+
+
+
+
+
+if __name__  ==  '__main__':
+    main()
