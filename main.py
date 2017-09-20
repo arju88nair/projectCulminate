3c3c85886e1a8db0a861f3c206aa40eebf1418cf
arju88nair <arju88nair@gmail.com>
2017-09-20 23:50:46 +0530

    Broken code

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import RAKE
 from calendar import timegm
 from datetime import datetime
 import hashlib
+from fuzzywuzzy import fuzz
+
 
 logging.basicConfig(filename='logger.log', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -32,8 +34,27 @@ class insertingClass:
         self.category = category
         self.source = source
 
-    def mainClassObj(self):
-        print(self.data['category'])
+    def individualInsertObj(self):
+        if self.data['category'] == "General":
+            print(self.data['title'])
+            if not db.generalMain.find({"uTag": str(self.data['uTag'])}).count() > 0:
+                # if db.generalMain.count() == 0:
+                #     insertDoc = db.generalMain.insert_one(self.data)
+                for document in db.generalMain.find():
+                    print(self.data['title'])
+                    print("\n")
+                    collision = fuzz.token_sort_ratio(
+                        str(self.data['title']), document['title'])
+                    print(collision)
+                    if int(collision) < 50:
+                        print("d")
+                        insertDoc = db.generalMain.insert_one(self.data)
+                        if insertDoc:
+                            logging.info('Insert new for general')
+                            logging.info('\n')
+                        else:
+                            logging.info('Error in insertion for general')
+                            logging.info('\n')
 
     def tempTablePush(self):
         """
@@ -90,52 +111,53 @@ def Type1parser(url, source, category, tag):
             itemArray['created_at'] = str(datetime.now())
             itemArray['uTag'] = hashlib.sha256(
                 str(item.title).encode('utf-8')).hexdigest()[:16]
-            array.append(itemArray)
+            individualInsert = insertingClass(itemArray, category, source)
+            individualInsert.individualInsertObj()
 
         # insertingClassObject=insertingClass(itemArray,category)
         # insertingClassObject.mainClassObj()
         # return "hi";
 
     # print(json.dumps(posts))
-    tempPush = insertingClass(array, category, url)
-    tempPush.tempTablePush()
 
+
+# main function
 
 def main():
     """  Calling the main class parser for appropriate  rss feeds """
 
     Type1parser("http://rss.cnn.com/rss/edition.rss", "CNN", "General", "Top")
-    Type1parser("http://rss.cnn.com/rss/edition_world.rss",
-                "CNN", "World", "Top")
-    Type1parser("http://rss.cnn.com/rss/edition_technology.rss",
-                "CNN", "Technology", "Top")
-    Type1parser("http://rss.cnn.com/rss/edition_space.rss",
-                "CNN", "Science", "Top")
-    Type1parser("http://rss.cnn.com/rss/edition_entertainment.rss",
-                "CNN", "Entertainment", "Top")
-    Type1parser("http://rss.cnn.com/rss/cnn_latest.rss",
-                "CNN", "General", "Latest")
+    # Type1parser("http://rss.cnn.com/rss/edition_world.rss",
+    #             "CNN", "World", "Top")
+    # Type1parser("http://rss.cnn.com/rss/edition_technology.rss",
+    #             "CNN", "Technology", "Top")
+    # Type1parser("http://rss.cnn.com/rss/edition_space.rss",
+    #             "CNN", "Science", "Top")
+    # Type1parser("http://rss.cnn.com/rss/edition_entertainment.rss",
+    #             "CNN", "Entertainment", "Top")
+    # Type1parser("http://rss.cnn.com/rss/cnn_latest.rss",
+    #             "CNN", "General", "Latest")
 
-    print("\n")
+    # print("\n")
 
-    Type1parser("http://feeds.bbci.co.uk/news/rss.xml",
-                "BBC", "General", "Top")
-    Type1parser("http://feeds.bbci.co.uk/news/world/rss.xml",
-                "BBC", "World", "Top")
-    Type1parser("http://feeds.bbci.co.uk/news/politics/rss.xml",
-                "BBC", "Politics", "Top")
-    Type1parser("http://feeds.bbci.co.uk/news/business/rss.xml",
-                "BBC", "Business", "Top")
-    Type1parser("http://feeds.bbci.co.uk/news/health/rss.xml",
-                "BBC", "Health", "Top")
-    Type1parser("http://feeds.bbci.co.uk/news/education/rss.xml",
-                "BBC", "Education", "Top")
-    Type1parser("http://feeds.bbci.co.uk/news/science_and_environment/rss.xml",
-                "BBC", "Science", "Top")
-    Type1parser("http://feeds.bbci.co.uk/news/technology/rss.xml",
-                "BBC", "Technology", "Top")
-    Type1parser("http://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml",
-                "BBC", "Entertainment", "Top")
+    # Type1parser("http://feeds.bbci.co.uk/news/rss.xml",
+    #             "BBC", "General", "Top")
+    # Type1parser("http://feeds.bbci.co.uk/news/world/rss.xml",
+    #             "BBC", "World", "Top")
+    # Type1parser("http://feeds.bbci.co.uk/news/politics/rss.xml",
+    #             "BBC", "Politics", "Top")
+    # Type1parser("http://feeds.bbci.co.uk/news/business/rss.xml",
+    #             "BBC", "Business", "Top")
+    # Type1parser("http://feeds.bbci.co.uk/news/health/rss.xml",
+    #             "BBC", "Health", "Top")
+    # Type1parser("http://feeds.bbci.co.uk/news/education/rss.xml",
+    #             "BBC", "Education", "Top")
+    # Type1parser("http://feeds.bbci.co.uk/news/science_and_environment/rss.xml",
+    #             "BBC", "Science", "Top")
+    # Type1parser("http://feeds.bbci.co.uk/news/technology/rss.xml",
+    #             "BBC", "Technology", "Top")
+    # Type1parser("http://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml",
+    #             "BBC", "Entertainment", "Top")
 
 
 if __name__ == '__main__':
