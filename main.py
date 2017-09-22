8d00d280f348a6c4c6b250c797bd763e3a543f95
arju88nair <arju88nair@gmail.com>
2017-09-22 22:35:19 +0530

    I did I did fixed the bug (more or less) ,new bugs

--- main.py
+++ main.py
@@ -45,22 +45,22 @@ class insertingClass:
 
         if self.data['category'] == "General":
             collectionInsert(db.General, "General", self.data)
-        # if self.data['category'] == "Technology":
-        #     collectionInsert(db.Technology, "Technology", self.data)
-        # if self.data['category'] == "Science":
-        #     collectionInsert(db.Science, "Science", self.data)
-        # if self.data['category'] == "Entertainment":
-        #     collectionInsert(db.Entertainment, "Entertainment", self.data)
-        # if self.data['category'] == "World":
-        #     collectionInsert(db.World, "World", self.data)
-        # if self.data['category'] == "Politics":
-        #     collectionInsert(db.Politics, "Politics", self.data)
-        # if self.data['category'] == "Business":
-        #     collectionInsert(db.Business, "Business", self.data)
-        # if self.data['category'] == "Health":
-        #     collectionInsert(db.Health, "Health", self.data)
-        # if self.data['category'] == "Education":
-        #     collectionInsert(db.Education, "Education", self.data)
+        if self.data['category'] == "Technology":
+            collectionInsert(db.Technology, "Technology", self.data)
+        if self.data['category'] == "Science":
+            collectionInsert(db.Science, "Science", self.data)
+        if self.data['category'] == "Entertainment":
+            collectionInsert(db.Entertainment, "Entertainment", self.data)
+        if self.data['category'] == "World":
+            collectionInsert(db.World, "World", self.data)
+        if self.data['category'] == "Politics":
+            collectionInsert(db.Politics, "Politics", self.data)
+        if self.data['category'] == "Business":
+            collectionInsert(db.Business, "Business", self.data)
+        if self.data['category'] == "Health":
+            collectionInsert(db.Health, "Health", self.data)
+        if self.data['category'] == "Education":
+            collectionInsert(db.Education, "Education", self.data)
 
 # Individual insertion fucntion
 
@@ -79,13 +79,14 @@ def collectionInsert(collectionName, tag, data):
             collision = fuzz.token_sort_ratio(
                 str(data['title']), document['title'])
             tags = str(data['uTag'])
-            if db.General.find_one(
+            if collectionName.find_one(
                     {'uTag': tags}, {'_id': 1}):
                 pass
             else:
                 insertDoc = collectionName.insert_one(data)
                 if insertDoc:
-                    logging.info('Insert new for ' + tag)
+                    logging.info('Insert new for ' + tag +
+                                 "In " + collectionName)
                     logging.info('\n')
                 else:
                     logging.info('Error in insertion for ' + tag)
@@ -121,7 +122,6 @@ def Type1parser(url, source, category, tag):
                 tagWordArray.append(word[0].title())
             itemArray = dict()
             itemArray['title'] = item.title
-            itemArray['summary'] = summarys
             itemArray['link'] = item.link
             itemArray['image'] = item.media_content[0]['url']
             itemArray['published'] = publishedTag
@@ -134,6 +134,7 @@ def Type1parser(url, source, category, tag):
                 str(item.title).encode('utf-8')).hexdigest()[:16]
             individualInsert = insertingClass(itemArray, category, source)
             individualInsert.individualInsertObj()
+    print(category + source)
 
 
 # main function
@@ -142,37 +143,36 @@ def main():
     """  Calling the main class parser for appropriate  rss feeds """
 
     Type1parser("http://rss.cnn.com/rss/edition.rss", "CNN", "General", "Top")
-    # Type1parser("http://rss.cnn.com/rss/edition_world.rss",
-    #             "CNN", "World", "Top")
-    # Type1parser("http://rss.cnn.com/rss/edition_technology.rss",
-    #             "CNN", "Technology", "Top")
-    # Type1parser("http://rss.cnn.com/rss/edition_space.rss",
-    #             "CNN", "Science", "Top")
-    # Type1parser("http://rss.cnn.com/rss/edition_entertainment.rss",
-    #             "CNN", "Entertainment", "Top")
-    # Type1parser("http://rss.cnn.com/rss/cnn_latest.rss",
-    #             "CNN", "General", "Latest")
 
-    # print("\n")
+    Type1parser("http://rss.cnn.com/rss/edition_world.rss",
+                "CNN", "World", "Top")
+    Type1parser("http://rss.cnn.com/rss/edition_technology.rss",
+                "CNN", "Technology", "Top")
+    Type1parser("http://rss.cnn.com/rss/edition_space.rss",
+                "CNN", "Science", "Top")
+    Type1parser("http://rss.cnn.com/rss/edition_entertainment.rss",
+                "CNN", "Entertainment", "Top")
+    Type1parser("http://rss.cnn.com/rss/cnn_latest.rss",
+                "CNN", "General", "Latest")
 
-    # Type1parser("http://feeds.bbci.co.uk/news/rss.xml",
-    #             "BBC", "General", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/world/rss.xml",
-    #             "BBC", "World", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/politics/rss.xml",
-    #             "BBC", "Politics", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/business/rss.xml",
-    #             "BBC", "Business", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/health/rss.xml",
-    #             "BBC", "Health", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/education/rss.xml",
-    #             "BBC", "Education", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/science_and_environment/rss.xml",
-    #             "BBC", "Science", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/technology/rss.xml",
-    #             "BBC", "Technology", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml",
-    #             "BBC", "Entertainment", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/rss.xml",
+                "BBC", "General", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/world/rss.xml",
+                "BBC", "World", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/politics/rss.xml",
+                "BBC", "Politics", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/business/rss.xml",
+                "BBC", "Business", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/health/rss.xml",
+                "BBC", "Health", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/education/rss.xml",
+                "BBC", "Education", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/science_and_environment/rss.xml",
+                "BBC", "Science", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/technology/rss.xml",
+                "BBC", "Technology", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml",
+                "BBC", "Entertainment", "Top")
 
 
 if __name__ == '__main__':
