94c6f7ec499d37fbf2164cd6b4e8b6bacf41d0f2
arju88nair <arju88nair@gmail.com>
2017-09-30 20:14:14 +0530

    HTTTPResponse

--- api.py
+++ api.py
@@ -1,110 +1,23 @@
-import asyncio
 import feedparser  # pip install feedparser
-from pymongo import MongoClient
-import json
-import re
-import datetime
-import logging
-import RAKE
-from calendar import timegm
-from datetime import datetime
-import hashlib
-from fuzzywuzzy import fuzz
-from bs4 import BeautifulSoup
+import time
+start = time.time()
 
 
-@asyncio.coroutine
-def Type1parser(url, source, category, tag):
-    """
-
-    This class handles all the feed parsing jobs initialted by the main function
-
-    """
-
+def compute(url):
     feed = feedparser.parse(url)
     array = []
     for item in feed['entries']:
-
-        summarys = ""
-        if 'summary' in item:
-            cleantext = BeautifulSoup(item.summary).text
-
-            summarys = cleantext
-        publishedTag = ""
-        if 'published' in item:
-            publishedTag = item.published
-        # if 'media_content' in item:
-            # takes stopwords as list of strings
-        Rake = RAKE.Rake('stopwords_en.txt')
-        words = Rake.run(item.title)
-        tagWordArray = []
-        for word in words:
-            tagWordArray.append(word[0].title())
-        itemArray = dict()
-        itemArray['title'] = item.title
-        itemArray['link'] = item.link
-        if 'media_content' in item:
-
-            itemArray['image'] = item.media_content[0]['url']
-        if 'media_thumbnail' in item:
-
-            itemArray['image'] = item.media_thumbnail[0]['url']
-
-        itemArray['published'] = publishedTag
-        itemArray['source'] = source
-        itemArray['type'] = tag
-        itemArray['category'] = category
-        itemArray['summary'] = summarys
-        itemArray['tags'] = tagWordArray
-        itemArray['created_at'] = str(datetime.now())
-        itemArray['uTag'] = hashlib.sha256(
-            str(item.title).encode('utf-8')).hexdigest()[:16]
-
-        individualInsert = insertingClass(itemArray, category, source)
-        individualInsert.individualInsertObj()
+        return item.title
 
 
-class insertingClass:
-    """
-    insertingClass does various sorting and inserting data to appropriate collections
-    @params data : contains the data
-    @params category : the category of the data
-    @params source :the source of the data
-
-     """
-
-    def __init__(self, data, category, source):
-        self.data = data
-        self.category = category
-        self.source = source
-
-    def individualInsertObj(self):
-        """
-
-        Classifying according to the category
-
-        """
-        print(self.source)
+def print_sum():
+    urls = ["http://feeds.reuters.com/reuters/INtopNews",
+            "http://feeds.reuters.com/reuters/INentertainmentNews"]
+    for url in urls:
+        print(compute(url))
 
 
-# Individual insertion fucntion
+if __name__ == '__main__':
+    print_sum()
 
-
-def got_result(future):
-
-    return future.result()
-
-
-loop = asyncio.get_event_loop()
-
-tasks = [
-    Type1parser("http://feeds.reuters.com/reuters/INtopNews",
-                "Reuters India", "General", "Top"),
-    Type1parser("https://www.theguardian.com/world/rss",
-                "The Guardian", "World", "Top")]
-
-# name = future1.add_done_callback(got_result)
-# age = future2.add_done_callback(got_result)
-
-loop.run_until_complete(asyncio.wait(tasks))
-loop.close()
+print ('It took', time.time() - start, 'seconds.')
--- test.py
+++ test.py
@@ -1,17 +1,40 @@
+# import asyncio
+# import feedparser  # pip install feedparser
+# import time
+# start = time.time()
+
+
+# async def compute(url):
+#     feed = feedparser.parse(url)
+#     array = []
+#     for item in feed['entries']:
+#         return item.title
+
+
+# async def print_sum(url):
+#     result = await compute(url)
+#     print(result)
+
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(asyncio.gather(print_sum("http://feeds.reuters.com/reuters/INtopNews"), print_sum("http://feeds.reuters.com/reuters/INentertainmentNews"))
+#                         )
+# loop.close()
+# print ('It took', time.time() - start, 'seconds.')
 import asyncio
-import requests
 
 
-async def main():
-    loop = asyncio.get_event_loop()
-    future1 = loop.run_in_executor(
-        None, requests.get, 'http://feeds.reuters.com/reuters/INtopNews')
-    future2 = loop.run_in_executor(
-        None, requests.get, 'https://www.theguardian.com/world/rss')
-    response1 = await future1
-    response2 = await future2
-    print(response1.text)
-    print(response2.text)
+async def factorial(name, number):
+    f = 1
+    for i in range(2, number + 1):
+        print("Task %s: Compute factorial(%s)..." % (name, i))
+        await asyncio.sleep(1)
+        f *= i
+    print("Task %s: factorial(%s) = %s" % (name, number, f))
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+loop.run_until_complete(asyncio.gather(
+    factorial("A", 2),
+    factorial("B", 3),
+    factorial("C", 4),
+))
+loop.close()
