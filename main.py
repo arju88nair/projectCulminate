75a39d772990688066ac90a7db0798c3b7aed2d8
arju88nair <arju88nair@gmail.com>
2017-10-02 00:39:26 +0530

    Added every url to threader array

--- main.py
+++ main.py
@@ -171,42 +171,42 @@ def main():
 def main():
     """  Calling the main class parser for appropriate  rss feeds """
 
-    # Type1parser("http://rss.cnn.com/rss/edition.rss", "CNN", "General", "Top")
+    Type1parser("http://rss.cnn.com/rss/edition.rss", "CNN", "General", "Top")
 
-    # Type1parser("http://rss.cnn.com/rss/edition_world.rss",
-    #             "CNN", "World", "Top")
-    # Type1parser("http://rss.cnn.com/rss/edition_technology.rss",
-    #             "CNN", "Technology", "Top")
-    # Type1parser("http://rss.cnn.com/rss/edition_space.rss",
-    #             "CNN", "Science", "Top")
-    # Type1parser("http://rss.cnn.com/rss/edition_entertainment.rss",
-    #             "CNN", "Entertainment", "Top")
-    # Type1parser("http://rss.cnn.com/rss/cnn_latest.rss",
-    #             "CNN", "General", "Latest")
+    Type1parser("http://rss.cnn.com/rss/edition_world.rss",
+                "CNN", "World", "Top")
+    Type1parser("http://rss.cnn.com/rss/edition_technology.rss",
+                "CNN", "Technology", "Top")
+    Type1parser("http://rss.cnn.com/rss/edition_space.rss",
+                "CNN", "Science", "Top")
+    Type1parser("http://rss.cnn.com/rss/edition_entertainment.rss",
+                "CNN", "Entertainment", "Top")
+    Type1parser("http://rss.cnn.com/rss/cnn_latest.rss",
+                "CNN", "General", "Latest")
 
-    # Type1parser("http://feeds.bbci.co.uk/news/rss.xml",
-    #             "BBC", "General", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/world/rss.xml",
-    #             "BBC", "World", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/politics/rss.xml",
-    #             "BBC", "Politics", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/business/rss.xml",
-    #             "BBC", "Business", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/health/rss.xml",
-    #             "BBC", "Health", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/education/rss.xml",
-    #             "BBC", "Education", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/science_and_environment/rss.xml",
-    #             "BBC", "Science", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/technology/rss.xml",
-    #             "BBC", "Technology", "Top")
-    # Type1parser("http://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml",
-    #             "BBC", "Entertainment", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/rss.xml",
+                "BBC", "General", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/world/rss.xml",
+                "BBC", "World", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/politics/rss.xml",
+                "BBC", "Politics", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/business/rss.xml",
+                "BBC", "Business", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/health/rss.xml",
+                "BBC", "Health", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/education/rss.xml",
+                "BBC", "Education", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/science_and_environment/rss.xml",
+                "BBC", "Science", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/technology/rss.xml",
+                "BBC", "Technology", "Top")
+    Type1parser("http://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml",
+                "BBC", "Entertainment", "Top")
 
 # Times of India
 
-    # Type1parser("http://timesofindia.indiatimes.com/rssfeedstopstories.cms",
-    #             "Times of India", "General", "Top")
+    Type1parser("http://timesofindia.indiatimes.com/rssfeedstopstories.cms",
+                "Times of India", "General", "Top")
 
 # New York Times
 
